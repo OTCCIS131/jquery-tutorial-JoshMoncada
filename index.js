0cab3b61972da5d62e279e842159e727e3325cbb
JoshMoncada <jm0184258@otc.edu>
2017-10-23 12:20:30 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+$(function () {
+    let data = {
+        el: '#app',
+        data: 
+        ppt: 60.00,
+        attendees: [
+            { name: 'Josh Moncada', email: 'joshmoncada@gmail.com'}
+        ]
+    };
+})
