4a99eeae3732103fe5e65b89a7758d0a24410372
JoshMoncada <jm0184258@otc.edu>
2017-10-24 01:12:33 -0500

    final

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {  
             
-    let data = {
-        ppt: 25.00,
+    var data = {
+        cost: 25.00,
         attendees: [ {name: 'Josh Moncada', email: 'joshmoncada@gmail.com'} ]
     };
 
@@ -10,47 +10,56 @@ $(document).ready(function () {
     }
 
     function addAttendee() {
-        $('.attendee-list').append($('script[data-template="attendee"]').text());
+        $('.attendee-list').append(
+            $('script[data-template="attendee"]').text());
+        syncRemoveButtons();    
+    }
+    
+    function syncRemoveButtons() {
+        if (getAttendeeCount()===1){
+        $('.attendee-list .attendee .remove-attendee').first().hide();
+        } 
+        else{
+        $('.attendee-list .attendee .remove-attendee').show();
+        }
     }
 
-    function syncPurchaseButton() {
+    function syncPurchaseButton(){
         $('#checkout-button span.amount').html('$' + data.cost * getAttendeeCount());
     }
 
-    //set ticket cost
-    $('#unit-price').html('$' + data.cost + ' each');
+    //events
+    $('.add-attendee').on('click', function (event) {
+        event.preventDefault();
+        addAttendee();
+        $(this).trigger('attendee:add');
+    }).on('attendee:add', 
+        function () {
+            syncPurchaseButton();
+            syncRemoveButtons();
+        });
 
+    //link event handler to remove button
+    $('#app').on('click','.attendee.remove-attendee',function(event){
+        event.precentDefault();
+        var $row = $(event.target).closest('.attendee.row');
+        $row.remove();
+        $('#app').trigger('attendee:remove');
+    });
+
+    $('#app').on('atendee:remove',function(){
+        syncPurchaseButton();
+        syncRemoveButtons();
+    });    
+
+    //set ticket cost
+    $('#unit-price').html('$' + data.cost + ' each');  
+    
     //initialize one attendee
     addAttendee();
     syncPurchaseButton();
 }); 
 
 
-function addAttendee() {
-    $('.attendee-list').append( $('script[data-template="attendee"]').text());
-    syncRemoveButtons();
-}
 
-function syncRemoveButtons() {
-    if (getAttendeeCount()===1){
-    $('.attendee-list .attendee .remove-attendee').first().hide();
-    } 
-    else{
-    $('.attendee-list .attendee .remove-attendee').show();
-    }
-}
 
-function syncPurchaseButton() {
-    $('#checkout-button span.amount').html('$' + data.cost * getAttendeeCount());
-}
-
-//Events
-$('.add-attendee').on('click', function (event) {
-        event.preventDefault();
-        addAttendee();
-        $(this).trigger('attendee:add');
-    }).on('attendee:add', 
-    function () {
-        syncPurchaseButton();
-        syncRemoveButtons();
-    });
